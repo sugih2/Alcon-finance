84fedd854d3f7983cea22b193456201794f1334d
Trouble Mind <arielpangestu141@gmail.com>
2024-12-03 22:37:16 +0700

    fixing pagination datatable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,12 +27,14 @@
     <script src="assets/moment/moment.min.js"></script> --}}
     {{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css"> --}}
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.min.css"> --}}
-    {{-- <link rel="stylesheet" href="https://cdn.datatables.net/2.1.2/css/dataTables.foundation.css"> --}}
+    <link href="https://cdn.datatables.net/v/dt/dt-2.1.8/datatables.min.css" rel="stylesheet">
+    <script src="https://cdn.datatables.net/v/dt/dt-2.1.8/datatables.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.2/css/dataTables.foundation.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.1.0/css/buttons.foundation.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    {{-- <script src="https://cdn.datatables.net/2.1.3/js/dataTables.js"></script> --}}
-    <script src="https://cdn.datatables.net/2.1.3/js/dataTables.foundation.js"></script>
+    <script src="https://cdn.datatables.net/2.1.3/js/dataTables.js"></script>
+    {{-- <script src="https://cdn.datatables.net/2.1.3/js/dataTables.foundation.js"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
